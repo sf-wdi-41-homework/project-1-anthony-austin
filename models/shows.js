7478662566ddeaa411f8482daa05037b6c4bf219
austino1 <32141431+austino1@users.noreply.github.com>
2017-10-17 14:05:09 -0700

    added routes and functionsfor create and delete requests

--- models/shows.js
+++ models/shows.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
+
+var ShowSchema = new Schema({
+  title: String,
+  imageUrl: String,
+  genre: String,
+  description: String,
+})
+
+var Shows = mongoose.model('Shows', ShowSchema);
+
+module.exports = Shows
